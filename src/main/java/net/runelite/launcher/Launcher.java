df843ab87608b2ba7bce803e8af577be768367d3
VeneOSRS <support@marefx.com>
2020-06-01 15:23:15 +0200

    Update repo

--- src/main/java/net/runelite/launcher/Launcher.java
+++ src/main/java/net/runelite/launcher/Launcher.java
@@ -73,9 +73,9 @@ public class Launcher
 	private static final File REPO_DIR = new File(RUNELITE_DIR, "repository2");
 	private static final File CRASH_FILES = new File(LOGS_DIR, "jvm_crash_pid_%p.log");
 	static final String LAUNCHER_BUILD = "https://raw.githubusercontent.com/vene-osrs/launcher/vene_osrs/build.gradle.kts";
-	private static final String CLIENT_BOOTSTRAP_STAGING_URL = "https://raw.githubusercontent.com/vene-osrs/hosting/master/bootstrap-staging.json";
-	private static final String CLIENT_BOOTSTRAP_STABLE_URL = "https://raw.githubusercontent.com/vene-osrs/hosting/master/bootstrap-stable.json";
-	private static final String CLIENT_BOOTSTRAP_NIGHTLY_URL = "https://raw.githubusercontent.com/vene-osrs/hosting/master/bootstrap-nightly.json";
+	private static final String CLIENT_BOOTSTRAP_STAGING_URL = "https://raw.githubusercontent.com/vene-osrs/hosting/vene_osrs/bootstrap-staging.json";
+	private static final String CLIENT_BOOTSTRAP_STABLE_URL = "https://raw.githubusercontent.com/vene-osrs/hosting/vene_osrs/bootstrap-stable.json";
+	private static final String CLIENT_BOOTSTRAP_NIGHTLY_URL = "https://raw.githubusercontent.com/vene-osrs/hosting/vene_osrs/bootstrap-nightly.json";
 	static final String USER_AGENT = "OpenOSRS/" + LauncherProperties.getVersion();
 	private static final boolean enforceDependencyHashing = true;
 	private static boolean nightly = false;
